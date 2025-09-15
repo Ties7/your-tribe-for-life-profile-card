8694c4bff9817b244b57f0026d1e3118b00df78b
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-09-15 14:42:02 +0200

    feat: svelte installatie #1

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { data } = $props();
+
+    console.log(data);
+
+</script>
+
+<h1>Welcome to Ties</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
