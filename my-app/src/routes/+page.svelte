1478ae4d9169f4932c42c5ee441afee05a9c96c7
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-09-16 15:03:21 +0200

    feat: html #6

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -4,4 +4,38 @@
     const member = data.members[0];
 </script>
 
-<h1>{member.name}</h1>
+<!-- <h1>{member.name}</h1> -->
+ 
+<main>
+    <section>
+        <h2>St<i>u</i>dio</h2>
+        <h1>TIESBEEFTINK</h1>
+        <picture>
+            <img src="" alt="">
+        </picture>
+    </section>
+
+    <article>
+        <picture>
+            <img src="" alt="">
+        </picture>
+        <h2></h2>
+        <p></p>
+    </article>
+
+    <article>
+        <picture>
+            <img src="" alt="">
+        </picture>
+        <h2></h2>
+        <p></p>
+    </article>
+
+    <section>
+        <h2>St<i>u</i>dio</h2>
+        <h1>TIESBEEFTINK</h1>
+        <picture>
+            <img src="" alt="">
+        </picture>
+    </section>
+</main>
