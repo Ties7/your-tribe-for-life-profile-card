e4ff9f36f52554d0ff891b37345c3acae9f8ae30
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-09-18 21:16:21 +0200

    fix, refactor: code refactor + positioning fix #6 #7

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,12 @@
  
 <main>
     <section class="hero">
-        <h2>St<i>u</i>dio</h2>
-        <h1>TIESBEEFTINK</h1>
-        <picture>
-            <img src={texture} alt="">
-        </picture>
+        <h2 class="subtitle">St<i>u</i>dio</h2>
+        <h1 class="title">TIESBEEFTINK</h1>
+        <picture class="image"><img src={texture} alt=""></picture>
     </section>
 
-    <article class="work">
+    <article class="content work">
         <picture>
             <img src="" alt="">
         </picture>
@@ -24,102 +22,109 @@
         <p>Ik werk 2 tot 3 dagen in de week als chef in een restaurant. Het is een Franse keuken met hier en daar wat andere invloeden.</p>
     </article>
 
-    <article class="hobby">
+    <article class="content hobby">
         <picture>
             <img src="" alt="">
         </picture>
         <h2>Hobby's</h2>
         <p>Ik ben naast school veel met muziek bezig. Ik produceer met 2 andere vrienden muziek voor artiesten in een studio. Voor nu is het vooral nog hobbymatig, maar het zou leuk zijn als ik hier later ook iets mee kan doen.</p>
     </article>
-
-    <!-- <section>
-        <h2>St<i>u</i>dio</h2>
-        <h1>TIESBEEFTINK</h1>
-        <picture>
-            <img src={texture} alt="">
-        </picture>
-    </section> -->
 </main>
 
 <style>
+    :root {
+        font-size: clamp(1.25rem, 3vw, 2.5rem);
+    }
+
+    @keyframes scrollTitle {
+        100% {
+            top: 0;
+        }
+    }
+
+    @keyframes scrollSubtitle {
+        100% {
+            translate: 0 5rem;
+        }
+    }
+
+    @keyframes scrollImage {
+        100% {
+            translate: 0 -10rem;
+        }
+    }
+
     .hero {
-        position: relative;
         display: flex;
-        flex-direction: column;
         justify-content: center;
         align-items: center;
-
-        h2,
-        picture {
-            position: absolute;
+        height: 100vh;
+    
+    .subtitle {
+        position: absolute;
+        top: calc(50vh - 7.5rem);
+        font-size: 3rem;
+        font-family: Calvino;
+        
+        @supports (animation-timeline: scroll()) {
+            & {
+                animation: scrollSubtitle linear both;
+                animation-timeline: scroll(root y);
+                animation-range: 0 75vh;
+            }
         }
-
-        h2 {
-            font-family: Calvino;
-            font-size: 10rem;
-            top: 9rem;
-        }
-
-        h1 {
-            font-size: 8rem;
-            font-weight: 400;
-            top: 17.5rem;
-            z-index: 100;
-            position: fixed;
-        }
-
-        picture {
-            top: 15rem;
-        }
-    }
-
-    main {
-        height: 400vh;
     }
     
-
-    @supports (animation-timeline: scroll()) {
-    body, .hero h2 {
-        animation: first linear both;
-        animation-timeline: scroll(block nearest);
-    }
- 
-        @keyframes first {
-            30% {
-                transform: translateY(10rem);
+    .title {
+        position: fixed;
+        top: calc(50vh - 4rem);
+        font-size: 2rem;
+        z-index: 2;
+        font-weight: 400;
+        
+        @supports (animation-timeline: scroll()) {
+            & {
+                animation: scrollTitle linear both;
+                animation-timeline: scroll(root y);
+                animation-range: 0 75vh;
             }
         }
     }
+    
+    .image {
+        width: 7rem;
+        aspect-ratio: 2 / 3;
 
-    @supports (animation-timeline: scroll()) {
-    body, .hero img {
-        animation: second linear both;
-        animation-timeline: scroll(block nearest);
-    }
- 
-        @keyframes second {
-            30% {
-                transform: translateY(-20rem);
+        z-index: 1;
+        
+        img {
+            /* object-fit: cover; */
+            width: 100%;
+            height: 100%;
+        }
+        
+        @supports (animation-timeline: scroll()) {
+            & {
+                animation: scrollImage linear both;
+                animation-timeline: scroll(root y);
+                animation-range: 0 75vh;
             }
         }
     }
-
-    @supports (animation-timeline: scroll()) {
-    body, .hero h1 {
-        animation: third linear both;
-        animation-timeline: scroll(block nearest);
     }
- 
-        @keyframes third {
-            20% {
-                transform: translateY(-17.5rem);
-            }
 
-            100% {
-                                transform: translateY(-17.5rem);
+    .content {
+        width: 10rem;
+    }
 
-            }
-        }
+    .work {
+        top: 140vh;
+        position: absolute;
+    }
+
+    .hobby {
+        top: 190vh;
+        position: absolute;
     }
  
 </style>
