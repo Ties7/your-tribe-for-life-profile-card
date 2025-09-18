36d1d9c24adea3f39228ea15905c2efbdce53537
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-09-18 23:10:54 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <!-- <h1>{member.name}</h1> -->
- 
+<div class="content-wrapper">
     <article class="content work">
         <picture>
             <img src="" alt="">
@@ -30,28 +30,31 @@
         <h2>Hobby's</h2>
         <p>Ik ben naast school veel met muziek bezig. Ik produceer met 2 andere vrienden muziek voor artiesten in een studio. Voor nu is het vooral nog hobbymatig, maar het zou leuk zijn als ik hier later ook iets mee kan doen.</p>
     </article>
+</div>
 
 <style>
+    .content-wrapper {
+        display: flex;
+        justify-content: center;
+        height: 200vh;
+    }
+    
     .content {
         width: 10rem;
+
+        h2, p {
+            font-size: .7rem;
+        }
     }
 
     .work {
         top: 140vh;
         position: absolute;
-
-        h2, p {
-            font-size: .5rem;
-        }
     }
 
     .hobby {
         top: 190vh;
         position: absolute;
-
-        h2, p {
-            font-size: .5rem;
-        }
     }
  
 </style>
